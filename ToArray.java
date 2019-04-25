5e94a1277c98536e5e987d4753fbb3abe380fa77
jcantillo94 <48455155+jcantillo94@users.noreply.github.com>
2019-04-24 23:34:04 -0400

    Update ToArray.java

--- ToArray.java
+++ ToArray.java
@@ -1,124 +1,84 @@
-import java.util.ArrayList;
 
-/**Class: ToArray
- * 
- * @author Danielle's Group
- * Receives a 10 letter word and a message and adds them to a large array
- */
-public class ToArray 
-{
-	
-	private String message;
-	private String tenLetterWord;
-	private int rows;
-	
-	public ToArray(String tenLetterWord, String message)
-	{
-		this.message = message.toUpperCase();
-		this.tenLetterWord = tenLetterWord;
-	}
+import java.util.Scanner;
 
-	public int getRows()
-	{
-		return rows;
-	}
+public class TestToArray {
 
-	/**Method: bigArray()
-	 * 
-	 * @return bigArray
-	 * Adds Strings to a two dimensional array
-	 */
-	public String[][] bigArray() 
-	{
-		NoBlanksOrPeriod fixItJesus = new NoBlanksOrPeriod(message);
-		String newMessage = fixItJesus.noSpaces();
-		rows = 1 + (newMessage.length() / 10);
-		String[][] bigArray = new String[rows][10];
-		char[] tenWord = tenLetterWord.toCharArray();
-		
-		int count = 0;
-		
-		//Adds the 10 letter word first
-		for(int i = 0; i < tenWord.length; i++)
-		{
-			bigArray[0][i] = Character.toString(tenWord[i]).toUpperCase();
-		}
-		
-		//Adds the message
-		for(int i = 1; i < rows; i++)
-		{
-			for(int j = 0; j < 10; j++)
-			{
-				bigArray[i][j] = Character.toString(newMessage.charAt(count));
-				count++;
-			}
-		}
-		
-		return bigArray;
-	}
-	// sorts characters of the first row of the array which in turn will sort the remaining characters of the row into columns.
-	public String[][] sortByLetter()
-	{
-		String[][] bigArray = bigArray();
-		boolean pass = true;
-		// first for-loop uses the first row of characters to compare to the characters of the other columns.
-		for (int k = 1; k < 10 && pass; k++)
-		{
-			pass = false;
-			//second for-loop uses the indexes compared up until the comparison of the middle two columns.
-			for(int i = 0; i < 10 - k; i++)
-			{
-				
-				if(bigArray[0][i].compareTo(bigArray[0][i+1]) > 0)
-				{
-					String[][] temp = new String[rows][1];
-					// Uses created temp array to sort characters and put them in the right position in bigArray
-					for(int j = 0; j < rows; j++)
-					{
-						temp[j][0] = bigArray[j][i];
-						bigArray[j][i] = bigArray[j][i+1];
-						bigArray[j][i+1] = temp[j][0];
-					}
-					pass = true;
-				}
-			}
-		}
-		return bigArray;
-	}
-	//
-	public String toString(String[][] array)
-	{
-		String message = "";
-		String spacedMessage = "";
-		//
-		for (int i = 0; i < 10; i++)
-		{
-			for(int j = 1; j < rows; j++)
-			{
-				message += array[j][i];
-			}
-		}
-		// Adding Spaces to the message every five charcters and returns the meesage.
-		for(int i = 0; i < message.length(); i = i + 5)
-		{
-			spacedMessage += message.substring(i, i + 5) + " ";
-		}
+    //Testing
+    public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
+        String message;
+        boolean keepGoing = true;
 
-		return spacedMessage;
-	}
+        while (keepGoing) {
+            Scanner input = new Scanner(System.in);
 
-	/** Method: printArray
-	  * @parm String[][] array
-	  * 
-	  * prints the contents of the multidimensional array
-	  */
-	public static printArray(String[][] array)
-	{
-		for (int i = 0; i < array.length; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        System.out.print(array[i][j] + " ");
+
+            System.out.println("Enter a message (Anything goes): ");
+            message = input.nextLine();
+
+            for (int k = 0; k < 2; k++) {
+
+                System.out.println("Enter two 10 letter words: ");
+
+                firstWord = input.nextLine();
+                secondWord = input.nextLine();
+
+                while (firstWord.length() != 10 || secondWord.length() != 10) {
+                    if (firstWord.equalsIgnoreCase("Stop") || message == null) {
+                        keepGoing = false;
+                        System.out.println("Good Bye");
+                        System.exit(0);
+                    } else {
+                        System.out.println("That's not a ten letter word...");
+                        System.out.println("Enter a 10 letter word: ");
+                        firstWord = input.nextLine();
+                        secondWord = input.nextLine();
                     }
-                System.out.println();
                 }
-	}
+
+                ToArray combinedMessage = new ToArray(firstWord, message);
+
+                System.out.println("First run of cypher that has been created! (1st row): 10 letter word / (Rest of the Rows): Message");
+                System.out.println();
+
+                combinedMessage.printArray(combinedMessage.bigArray());
+
+
+                String[][] sorted = combinedMessage.sortByLetter(combinedMessage.bigArray());
+                System.out.println();
+
+                System.out.println("Cypher after columns have been sorted (INCLUDES FIRST ROW): ");
+                System.out.println();
+                combinedMessage.printArray(sorted);
+
+                System.out.println();
+
+                System.out.println("Five letter Strings retrieved from sorted columns: ");
+                System.out.println(combinedMessage.toString(sorted));
+
+                System.out.println();
+
+                String newMessage = combinedMessage.toString(sorted);
+
+                ToArray secondRun = new ToArray(secondWord, newMessage);
+
+                System.out.println("Second run of cypher!");
+
+                        String[][] secondSort = secondRun.sortByLetter(secondRun.bigArray());
+
+                secondRun.printArray(secondSort);
+
+                System.out.println("Five letter Strings retrieved from sorted columns: ");
+                System.out.println(secondRun.toString(secondSort));
+                System.out.println("Double Encryption completed!");
+
+                System.out.println();
+                System.out.println("TIME TO ENCRYPT A NEW MESSAGE!");
+                System.out.println("If you dont want to continue making another encryption, leave message empty and/or type 'stop' for 10 letter word)");
+
+            }
+
+        }
+    }
 }
