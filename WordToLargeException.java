3d894e334c44e8b3eb40818a5c4d9d9e0dcb87f5
ddavis60 <43051471+ddavis60@users.noreply.github.com>
2019-04-25 10:03:20 -0400

    Delete WordToLargeException.java

--- WordToLargeException.java
+++
@@ -1,9 +0,0 @@
-/** Class: WordToLargeException
- * 
- * @author Danielle's Group
- * Exception to make sure the ten letter word has 10 letters
- */
-public class WordToLargeException extends Exception
-{
-	
-}
